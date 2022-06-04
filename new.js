ddb7b1dbbe09792c22cf033ca211fd26cec86842
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-06-03 17:49:35 -0700

    added structure to index.html and rework logic in new.js

--- new.js
+++ new.js
@@ -1,13 +1,43 @@
-function playRound(playerSelection, computerSelection) {
-    function computerPlay(0, 2) {
-        0 = Rock; 
-        1 = Paper;
-        2 = Scissors;
-        return Math.floor(Math.random(3));
+function playRound(userSelection, computerSelection) {
+
+const userPlay = prompt("Rock, Paper, or Scissors?");
+const computerPlay = Math.random();
+    if (computerPlay < 0.34) {
+        computerPlay = "Rock";
+    } else if (computerPlay <= 0.67) {
+        computerPlay = "Paper";
+    } else {
+        computerPlay = "Scissors";
     }
 
-    computerPlay();
-    // your code here!
-  
-return (playRound(playerSelection, computerSelection));
-}
+const compare = function(answer1, answer2) {
+    if(answer1 === answer2) {
+        return "Tie!";
+    }
+    if(answer1 === "Rock") {
+        if(answer2 === "Scissors") {
+        return "Rock Wins!";
+    } else {
+        return "Paper Wins!"
+        }
+    }
+    if(answer1 === "Paper") {
+        if (answer2 === "Rock") {
+            return "Paper Wins!";
+        } else {
+            if(answer2 === "Scissors") {
+                return "Scissors Wins!";
+            }
+        }
+        if(answer1 === "Scissors") {
+            if(answer2 === "Rock") {
+                return "Rock Wins!";
+            } else {
+                if(answer2 === "Paper") {
+                    return "Scissors Wins!";
+                }
+            }
+        }
+    };
+
+console.log(playRound(userSelection, computerSelection));
